81b02e8034b5bf304f8635be5018720133fbaccf
SirAlexGBE <Bindasjake14@Gmail.com>
2025-11-07 21:08:29 +0545

    auth services made

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -8,6 +8,7 @@ import {Input} from "@/components/ui/input";
 import {Label} from "@/components/ui/label";
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import {authService} from "@/services/authservices";
 
 export default function SignupPage() {
   const [isSubmitting, setIsSubmitting] = useState(false);
